867e64333546170ffc7f847821612cbc0049b788
Sergio Veloza <velozasergio@gmail.com>
2020-01-23 01:29:13 -0500

    0.0.0.2
    * Esta version permite el registro de usuarios "de momento no guarda la información"
    * Se hacen pruebas con GitHub Actions
    * Se registra el proyecto y se integra con Google Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBYNmiCwKlXqkywsIEeN0JbRyZnDI3WCMI",
+    authDomain: "pamidoro-db.firebaseapp.com",
+    databaseURL: "https://pamidoro-db.firebaseio.com",
+    projectId: "pamidoro-db",
+    storageBucket: "pamidoro-db.appspot.com",
+    messagingSenderId: "752550683357",
+    appId: "1:752550683357:web:1a8399a8a83f5ec93d3134",
+    measurementId: "G-KL1JGTGBJ9"
+}
